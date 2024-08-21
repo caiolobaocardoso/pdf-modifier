9f37dd7c20101cfd7f0370275486fa9a846692e3
Caio Cardoso <caiolobaocardoso@gmail.com>
2024-08-20 22:05:10 -0300

    update

--- .ocr/bin/activate.fish
+++ .ocr/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/caio/workspace/psswd-sender/.ocr"
+set -gx VIRTUAL_ENV "/home/caio/workspace/pdf-modifier/.ocr"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
